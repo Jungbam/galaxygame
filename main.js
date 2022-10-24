4288bb521f9a72f8c9976baa7fe33bec9486201d
Jungbam <108196588+Jungbam@users.noreply.github.com>
2022-10-24 14:55:43 +0900

    createBullet() 함수 추가

--- main.js
+++ main.js
@@ -15,7 +15,8 @@ document.body.appendChild(canvas)
 // 우주선의 y값 초기값은 전체 캔버스 x값 - 캔버스의 중간 값에서 우주선 절반값이 빠진 값
 let spaceshipX = canvas.width / 2 - 12
 let spaceshipY = canvas.height - 24
-
+// 총알이 만들어지면 배열 []을 render한다.
+let bulletList = []
 let backgroundImg, spaceshipImg, bulletImg, enemyImg
 
 function loadImge() {
@@ -36,7 +37,6 @@ const setupkeyboardListner = () => {
   // 키가 누르는 순간 해당 키 값을 트루로 만들어주자.
   document.addEventListener('keydown', function (e) {
     // 방향키의 키를 따보자 : 오른쪽 39, 왼쪽 37, 위로 38, 아래 40
-    // console.log(e.keyCode) // 무슨키가 눌렸는지 확인하려면 : event.keyCode
     keysdown[e.keyCode] = true
   })
   // 키를 떼는 순간 해당 키 값을 삭제한다.
@@ -66,6 +66,10 @@ const render = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height) // canvas에 렌더링 된 것들을 초기화하는 작업 / 하지 않았을 때 기존 그림들이 같이 렌더링되는 문제 발생.
   ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height) // drawImage(image, dx, dy) => 이미지는 필수, 2,3번 매개변수는 좌표값
   ctx.drawImage(spaceshipImg, spaceshipX, spaceshipY) // 캔버스에 우주선 값을 그려준다.
+  // 총알이 만들어진만큼 총알을 render 한다.
+  for (let i = 0; i < bulletList.length; i++) {
+    ctx.drawImage(bulletImg, bulletList[i].x, bulletList[i].y)
+  }
 }
 
 const main = () => {
@@ -82,9 +86,21 @@ main()
 // 스페이스바를 여러번 누르면 여러개의 총알이 발사된다. => 총알을 누르는 횟수만큼 요소가 만들어질 배열을 만들자.
 const createBullet = () => {
   // 스페이스가 눌렸을 때에 대한
-  console.log('in')
+  let bullet = new Bullet() // new가 뭐냐고 함수를 새로 만든다.
+  bullet.init()
 }
+
 // 각 총알은 좌표값(x,y)을 갖고 있어야 한다. => class화(constructor)
-// 총알을 누르는 배열 []을 render한다.
+function Bullet() {
+  this.x = 0
+  this.y = 0
+
+  this.init = function () {
+    this.x = spaceshipX
+    this.y = spaceshipY
+    console.log(this.x, this.y)
+    bulletList.push(this)
+  }
+}
 
 // keyup 이벤트에 스페이스를 눌렀을 때에 대한 정의를 해준다.
